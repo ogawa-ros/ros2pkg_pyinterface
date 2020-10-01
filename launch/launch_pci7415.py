3fe6c7677560a3c65ef7dc7e352d33d4d92cbfca
ogawahideo <ogawahideo@ogawahideonoMacBook-Air.local>
2020-10-01 18:40:52 +0900

    launch_pci7415.py

--- launch/launch_pci7415.py
+++ launch/launch_pci7415.py
@@ -5,14 +5,14 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='ros2pkg_pyinterface',
-            node_executable='launch_pci7415',
+            node_executable='pci7415',
             parameters=[
                 {"rsw_id" : "0" },
                 {"do_conf" :"[1, 1, 1, 1]" },
 
                 {"use_axis" :"xyu" },
 
-                {"x_pulse_conf" :"{'PULSE': 1, 'OUT': 0, 'DIR': 0, 'WAIT': 0, 'DUTY': 0}" },
+            #    {"x_pulse_conf" :"{'PULSE': 1, 'OUT': 0, 'DIR': 0, 'WAIT': 0, 'DUTY': 0}" },
                 {"x_mode" :"jog"} ,
                 {"x_clock" :"59"} ,
                 {"x_acc_mode" :"acc_normal" },
@@ -22,7 +22,7 @@ def generate_launch_description():
                 {"x_dec" : "50"},
                 {"x_step" :"1" },
 
-                {"y_pulse_conf" :"{'PULSE': 1, 'OUT': 0, 'DIR': 0, 'WAIT': 0, 'DUTY': 0}"},
+                #{"y_pulse_conf" :"{'PULSE': 1, 'OUT': 0, 'DIR': 0, 'WAIT': 0, 'DUTY': 0}"},
                 {"y_mode" :"jog" },
                 {"y_clock" :"59"} ,
                 {"y_acc_mode":"acc_normal" },
@@ -32,7 +32,7 @@ def generate_launch_description():
                 {"y_dec" :"50" },
                 {"y_step" :"1" },
 
-                {"u_pulse_conf" :"{'PULSE': 1, 'OUT': 1, 'DIR': 1, 'WAIT': 0, 'DUTY': 0}" },
+                #{"u_pulse_conf" :"{'PULSE': 1, 'OUT': 1, 'DIR': 1, 'WAIT': 0, 'DUTY': 0}" },
                 {"u_mode" :"ptp"},
                 {"u_clock" :"299"},
                 {"u_acc_mode" :"acc_normal" },
