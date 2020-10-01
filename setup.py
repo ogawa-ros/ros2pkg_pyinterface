cb935a68abc45e6130d9f9f8c2618eb665f1cc56
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2020-10-01 06:48:22 +0000

    first

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'ros2pkg_pyinterface'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='telescopio',
+    maintainer_email='s_ta.matsumoto@p.s.osakafu-u.ac.jp',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
